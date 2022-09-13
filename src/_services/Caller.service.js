6d14e0c3558cc2e7d18a0bfba38fa6780aae2ff5
Kevin <kvanconingsloo@gmail.com>
2022-09-13 19:08:20 +0200

    logout

--- src/_services/Caller.service.js
+++ src/_services/Caller.service.js
@@ -3,19 +3,32 @@
 import axios from 'axios'
 import { accountService } from './account.service'
 
+//import de router pour que les redirections soient plus rapide que $router
+import router from '../router'
+
 
 const Axios = axios.create({
     baseURL: 'http://localhost:8888'
 })
 
-Axios.interceptors.request.use(reqeuest => {
+Axios.interceptors.request.use(request => {
 
     if (accountService.isLogged) {
-        reqeuest.headers.Authorization = 'Bearer '+ accountService.getToken
+        request.headers.Authorization = 'Bearer '+ accountService.getToken
     }
 
-    return reqeuest
+    return request
 
+})
+
+Axios.interceptors.response.use(response => {
+    return response
+}, error => {
+    console.log(error.response.status)
+    if (error.response.status == 401) {
+        accountService.logout()
+        router.push('/login')
+    }
 })
 
 export default Axios
